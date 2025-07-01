cf37ee0899946a1c2a8812e9e86b01e31335905b
Luis Calleja <lecn99@hotmail.com>
2025-06-30 21:52:03 -0400

    committing all new stuff

--- container_credentials.py
+++ container_credentials.py
@@ -5,25 +5,32 @@
 
 
 #create a map of docker container credentials by system name
+import platform
 
-def return_credentials():
-    import platform 
+class Credentials:
 
     credentials_map = {
-    'mofongo':{'user':'root','pass':'salmon01','database':'membership','port':3306,'host':'172.17.0.2'}
+    'mofongo':{'user':'root','pass':'salmon01','database':'membership','port':3306,'host':'172.17.0.2'},
+    'candela':{'user':'root','pass':'salmon01','database':'membership','port':3306,'host':'172.17.0.2'}
     }
 
-    #retrieve computer name
     comp_name = platform.node()
 
-    if 'mofongo' in comp_name:
-        treated_name = 'mofongo'
-    elif 'luis' in comp_name:
-        treated_name = 'luisito'
-    else:
-        raise KeyError
+    def get_credentils_map(self):
+        return(Credentials.credentials_map)
     
-    #return a dictionary
-    return credentials_map[treated_name]
+    def retrieve_credentials(self):
+        if 'candela' in Credentials.comp_name:
+            treated_name = 'candela'
+        
+        elif 'mofongo' in Credentials.comp_name:
+            treated_name = 'mofongo'
+        elif 'luis' in Credentials.comp_name:
+            treated_name = 'luisito'    
+        
+        else:
+            raise KeyError
+        
+        return Credentials.credentials_map[treated_name]
     
 
